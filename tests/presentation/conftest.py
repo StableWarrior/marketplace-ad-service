3d57d58ea69926f93e33d1e736215c606e763522
Gleb Kibyakov <33168493+StableWarrior@users.noreply.github.com>
2026-06-01 17:28:15 +1000

    Update conftest.py

--- tests/presentation/conftest.py
+++ tests/presentation/conftest.py
@@ -26,8 +26,8 @@ _test_settings = Settings(
     postgres_username="fake",
     jwt_secret=JWT_SECRET,
     jwt_algorithm=JWT_ALGORITHM,
-    kafka_bootstrap_servers="localhost:9092",
-    kafka_topic_ads="ads",
+    kafka_brokers="localhost:9092",
+    kafka_topic_marketplace_ads="ads",
 )
 
 
