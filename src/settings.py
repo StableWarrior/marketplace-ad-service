50464192f05a67bf84b60e3d73a4fc5b61b4ee28
Gleb Kibyakov <33168493+StableWarrior@users.noreply.github.com>
2026-06-01 17:23:03 +1000

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -11,8 +11,8 @@ class Settings(BaseSettings):
     postgres_username: str
     jwt_secret: str = "change-me"
     jwt_algorithm: str = "HS256"
-    kafka_bootstrap_servers: str = "localhost:9092"
-    kafka_topic_ads: str = "ads"
+    kafka_brokers: str = "localhost:9092"
+    kafka_topic_marketplace_ads: str = "ads"
     auth_service_url: str = "http://localhost:8000"
 
     @property
